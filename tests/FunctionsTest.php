6614bff4fa68c322484dbdd661fe445ac3c16d29
ng3rdstmadgke <ng3rdstmadgke@gmail.com>
2018-11-11 20:51:45 +0900

    add bug

--- tests/FunctionsTest.php
+++ tests/FunctionsTest.php
@@ -6,6 +6,6 @@ use PHPUnit\Framework\TestCase;
 
 final class FunctionsTest extends TestCase {
   public function testSayHello() {
-    $this->assertEquals("Hello", Functions::sayHello());
+    $this->assertEquals("hello", Functions::sayHello());
   }
 }
